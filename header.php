eca207cc6673305a5f16fadf186ac880d7dbf7cd
Mike Curren <mcurren@stratabeat.com>
2018-02-05 10:12:55 -0600

    changed logo svg icon

--- header.php
+++ header.php
@@ -10,13 +10,21 @@ require_once( dirname(__FILE__) . '/functions.php' ); ?>
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<style type="text/css">
+		.navbar-brand img {
+      transition: transform 0.23s;
+		}
+		.navbar-brand:hover img {
+			transform: rotate(180deg);
+		}
+	</style>
 </head>
 <body>
 
 <header id="masthead" class="">
 	<nav class="navbar navbar-expand-md navbar-dark bg-info box-shadow">
 	  <a class="navbar-brand" href="/">
-	  	<img src="harvest.svg" width="30" height="30" class="d-inline-block align-top" alt="harvest horn of plenty"> 
+	  	<img src="hourglass.svg" width="33" height="30" class="d-inline-block align-top mr-0" alt="harvest horn of plenty"> 
 	  	Weekly Bounty
 	  </a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
