a907a5ba5d22923f6c6e9f07c348e9cd3235ac3f
Mike Curren <mcurren@stratabeat.com>
2018-02-18 14:45:15 -0600

    Layout fixes for Today view

--- today.php
+++ today.php
@@ -42,51 +42,56 @@ require_once( dirname(__FILE__) . '/header.php' ); ?>
     <div class="row">
       <div class="col-sm">
 
-        <nav class="btn-group mb-3" role="group" aria-label="Next/Previous Navigation">
-          <a class="btn btn-link" href="<?= $prev_url ?>" title="Day before this">&larr;</a>
-          <span class="btn"><h5 class="m-0"><?php echo date('D, M j', $today ); ?></h5></span>
-          <?php if( $next_url ) : ?>
-            <a class="btn btn-link" href="<?= $next_url ?>" Title="Day after this">&rarr;</a>
-          <?php endif; ?>
-        </nav>
+        <div class="row">
+          <nav class="btn-group mb-3 col-sm" role="group" aria-label="Next/Previous Navigation">
+            <a class="btn btn-link" href="<?= $prev_url ?>" title="Day before this">&larr;</a>
+            <span class="btn"><h5 class="m-0"><?php echo date('D, M j', $today ); ?></h5></span>
+            <?php if( $next_url ) : ?>
+              <a class="btn btn-link" href="<?= $next_url ?>" Title="Day after this">&rarr;</a>
+            <?php endif; ?>
+          </nav>
+        </div>
 
         <?php 
         // get harvest data
         $result = $api->getDailyActivity( $day_of_year, $year );
         if( $result->isSuccess() ) :
         	$total_hours = 0; ?>
-          <div class="d-flex flex-wrap d-flex justify-content-between align-items-stretch">
+          <div class="row d-flex flex-wrap d-flex">
             <?php foreach( $result->data->dayEntries as $entry ) : ?>
-              <!-- <div class="col-6"> -->
-              <section class="card mb-4">
-                <header class="card-header">
-                  <h2 class="text-dark h3"><?php echo $entry->get( 'project' ); ?></h2>
-                  <p class="text-dark m-0"><?php echo $entry->get( 'client' ); ?></p>
-                </header>
-                <div class="card-body">
-                  <p class="text-dark">
-                    <span class="display-4 text-primary"><?php echo $entry->get( 'hours' ); ?></span> 
-                    <small>hrs</small>
-                  </p>
-                  <p class="text-muted font-weight-light m-0">
-                    <?php echo $entry->get( 'notes' ); ?>
-                  </p>
-                </div>
-              </section>
-              <!-- </div> -->
+              <div class="col-md-6 col-lg-4 mb-4">
+                <section class="card align-self-stretch">
+                  <header class="card-header">
+                    <h2 class="text-dark h4"><?php echo $entry->get( 'project' ); ?></h2>
+                    <p class="text-dark m-0"><?php echo $entry->get( 'client' ); ?></p>
+                  </header>
+                  <div class="card-body">
+                    <p class="text-dark">
+                      <span class="display-4 text-primary"><?php echo $entry->get( 'hours' ); ?></span> 
+                      <small>hrs</small>
+                    </p>
+                    <p class="text-muted font-weight-light m-0">
+                      <?php echo $entry->get( 'notes' ); ?>
+                    </p>
+                  </div>
+                </section>
+              </div>
 
               <?php 
               // add $entry hours to daily total
               $total_hours += $entry->get( 'hours' );
 
             endforeach; ?>
-          </div>
-          <section class="mb-4">
-            <h1 class="">Daily Total</h1>
-            <p class="">
-              <span class="display-4 text-primary"><?= $total_hours ?></span> <span>hrs</span>
-            </p>
-          </section>
+          </div><!-- .row -->
+
+          <div class="row">
+            <section class="mb-4 col-sm">
+              <h1 class="">Daily Total</h1>
+              <p class="">
+                <span class="display-4 text-primary"><?= $total_hours ?></span> <span>hrs</span>
+              </p>
+            </section>
+          </div><!-- .row -->
 
         <?php endif; ?>
 
